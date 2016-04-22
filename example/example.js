d5da9f6b1eb076715bb3243a0ae2fbc2dd387c2a
Troy Whiteley <troy@somany.us>
2016-04-21 20:33:23 -0700

    Add separate methods for binding actions and elements

--- example/example.js
+++ example/example.js
@@ -1,5 +1,32 @@
 class Example extends Domodule {
+  constructor(el) {
+    super(el);
+
+    // this gets added after events are bound and events named
+    const newEl = document.createDocumentFragment();
+
+    for (let i = 0; i < 5; i++) {
+      const temp = document.createElement('button');
+
+      temp.dataset.name = `test${i}`;
+      temp.dataset.action = 'click';
+      temp.dataset.actionIndex = i;
+      temp.textContent = `Click me! ${i}`;
+
+      newEl.appendChild(temp);
+    }
+
+    this.el.appendChild(newEl);
+    this.setupActions();
+    this.setupNamed();
+  }
   testMouseOver(el, event, values) {
     console.log(el, event, values);
+    console.log(this.els);
+  }
+
+  click(el, event, values) {
+    console.log('clicked', el, values);
+    return false;
   }
 }
