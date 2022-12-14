73f1b86649ddad1c58109241fe8fa30f55753e55
ApplianceJohn <thednbgremlin@gmail.com>
2022-12-13 19:37:20 -0500

    moved back to /lib folder

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   settings: {
     "import/resolver": {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: [".js", ".jsx"],
       },
     },
   },
